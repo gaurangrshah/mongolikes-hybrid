64cb52f3b84c344c8a82a35b67c3e55052a0145f
gaurangrshah <gaurang.r.shah@gmail.com>
2021-08-03 16:48:33 -0400

    docs: retro

--- src/backend/middleware/verify.js
+++ src/backend/middleware/verify.js
@@ -2,6 +2,10 @@ import jwt from "next-auth/jwt";
 // @NOTE: cannot seem to access session on the server - works with getssp but not here
 import { getSession } from "next-auth/client";
 export const verify = async (req, res, next) => {
+  // @NOTE: NOT IMPLEMENTED, CANNOT GET SESSSION WHEN CALLING API ROUTES FROM GETSERVERSIDEPROPS
+  // DIRECTLY ACCESSING WORKINGS BY NAVIGATING IN BROWSER TO AUTHORIZED ENDPOINT
+  // BUT CANNOT SEEM TO GET THE SAME RESULT FROM GETSERVERSIDEPROPS
+
   try {
     // console.log("verifying session");
     // const payload = await jwt.getToken({ req, secret: process.env.SECRET });
