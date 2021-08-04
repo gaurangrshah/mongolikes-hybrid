15fdcce50a76ab3de92d48836a68bbcf7683c520
gaurangrshah <gaurang.r.shah@gmail.com>
2021-08-04 04:16:07 -0400

    chore: handle errors on create route

--- src/pages/api/post/create.js
+++ src/pages/api/post/create.js
@@ -11,6 +11,8 @@ const handler = nc({ onError })
   .use(verify) // 🔒 used by client api
   .use(middleware)
   .post(async (req, res) => {
+    if (!user) return res.status(401).json({ message: "Unauthorized" });
+
     const post = await createPost(
       isValidJson(req.body) ? JSON.parse(req.body) : req.body,
       req?.user?.sub
