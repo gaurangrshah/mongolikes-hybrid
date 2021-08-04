7b1d89de43e851fe486d56d547b71f8b6d35d9b0
gaurangrshah <gaurang.r.shah@gmail.com>
2021-08-03 23:42:17 -0400

    fix: update verify: only used on client side route

--- src/backend/middleware/verify.js
+++ src/backend/middleware/verify.js
@@ -1,30 +1,23 @@
 import jwt from "next-auth/jwt";
-// @NOTE: cannot seem to access session on the server - works with getssp but not here
-import { getSession } from "next-auth/client";
+
 export const verify = async (req, res, next) => {
-  // @NOTE: NOT IMPLEMENTED, CANNOT GET SESSSION WHEN CALLING API ROUTES FROM GETSERVERSIDEPROPS
+  // @NOTE: NOT IMPLEMENTED ON ALL ROUTES,
+  // CANNOT GET SESSSION WHEN CALLING API ROUTES FROM GETSERVERSIDEPROPS
   // DIRECTLY ACCESSING WORKINGS BY NAVIGATING IN BROWSER TO AUTHORIZED ENDPOINT
   // BUT CANNOT SEEM TO GET THE SAME RESULT FROM GETSERVERSIDEPROPS
 
+  // @NOTE: see pages/post/create for working example - from clientside
+
   try {
-    // console.log("verifying session");
-    // const payload = await jwt.getToken({ req, secret: process.env.SECRET });
-    // console.log("🚀 | file: verify.js | line 7 | payload", payload);
-    // if (payload) {
-    //   console.log("token verified", payload.sub);
-    //   req.user = payload;
-    //   next();
-    // } else throw new Error("must be authenticated");
-    // console.log(req.cookie);
-    // const session = await getSession({ req });
-    // console.log("🚀 | file: verify.js | line 14 | session", session);
-    // if (session) {
-    //   console.log("session verified", session.user._id);
-    //   req.user = session.user;
-    //   next();
-    // } else throw new Error("must be authenticated");
+    console.log("--- verifying session");
+    const payload = await jwt.getToken({ req, secret: process.env.SECRET });
+    if (payload) {
+      console.log("--- token verified");
+      req.user = payload;
+      next();
+    } else throw new Error("!Error must be authenticated");
   } catch (err) {
-    console.log("token not found");
+    console.log("--- token not found");
     next(err);
   }
 };
