cb6bc878d763195f45f4f721fd77ab0a1a899fc5
gaurangrshah <gaurang.r.shah@gmail.com>
2021-08-04 22:54:30 -0400

    feat: adds standardized msgs

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 export * from "./flatten-objects";
 export * from "./is-valid-json";
 export * from "./json-compare";
+export * from "./messages";
 export * from "./react";
 export * from "./swr";
---
+++ src/utils/messages.js
@@ -0,0 +1,13 @@
+export const messages = {
+  postcreated: "🎉  Success! Post Created! 🎉",
+  postpublished: "🎉  Success! Post Published! 🎉",
+  postdeleted: "🎉  Success! Post Deleted! 🎉",
+  notmodified: "The content was not modified.",
+  noop: "Could not complete this reqeust, please try again.",
+  success: "Your request was successful.",
+  unknown: "An unknown error has occurred.",
+  unauthorized: "You are not authorized to perform this action.",
+  forbidden: "You do not have sufficient permissions.",
+  tryagain:
+    "There was a problem with your request, please refresh the page or try again.",
+};
